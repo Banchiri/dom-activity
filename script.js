9622d44a33d1acf8fb43ca8b602c50a77447e26c
sandrabanchiri <sandrabanchiri39@gmail.com>
2024-07-19 16:40:03 +0300

    new file

---
+++ script.js
@@ -0,0 +1,19 @@
+const body = document.body
+const div = document.querySelector("div")
+const spanhey = document.querySelector("#hey")
+const spanBye = document.querySelector("#Bye")
+// spanhey.remove()
+// // div.append(spanBye)
+// div.appendChild(spanhey)
+// //div is the parent of span tag.
+// //modify actual attributes of an element
+console.log(spanhey.dataset.shorterName)
+spanhey.style.color = "red"
+
+
+
+
+
+
+
+
